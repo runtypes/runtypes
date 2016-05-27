5f2ac253d85e3528997519d589c2bd2e2a3dbee1
Tom Crockett <pelotom@gmail.com>
2016-05-27 02:24:54 -0700

    Minor refactor

--- src/record.js
+++ src/record.js
@@ -34,44 +34,42 @@ export default (spec) => {
   }
 
   return (obj) => {
-    if (!Settings.check)
-      return obj
+    if (Settings.check) {
+      const missingKeys = keyDiff(spec, obj)
+      if (missingKeys.length > 0)
+        throw new TypeError(errMissingRecordFields(missingKeys))
 
-    const missingKeys = keyDiff(spec, obj)
-    if (missingKeys.length > 0)
-      throw new TypeError(errMissingRecordFields(missingKeys))
+      const extraKeys = keyDiff(obj, spec)
+      if (extraKeys.length > 0)
+        throw new TypeError(errExtraneousRecordFields(extraKeys))
 
-    const extraKeys = keyDiff(obj, spec)
-    if (extraKeys.length > 0)
-      throw new TypeError(errExtraneousRecordFields(extraKeys))
+      for (const key in obj) {
+        const val = obj[key]
+        const type = spec[key]
+        const errMsg = checkType(val, type)
+        if (errMsg)
+          throw new TypeError(errBadRecordFieldValue(val, key, errMsg))
+      }
 
-    for (const key in obj) {
-      const val = obj[key]
-      const type = spec[key]
-      const errMsg = checkType(val, type)
-      if (errMsg)
-        throw new TypeError(errBadRecordFieldValue(val, key, errMsg))
-    }
+      // If our environment supports proxies, use them to prevent accessing
+      // nonexistent fields
+      if (typeof Proxy === 'function') {
+        return new Proxy(Object.freeze(obj), {
+          get(_, key) {
+            if (key in obj)
+              return obj[key]
+            else
+              throw new TypeError(errGetNonexistentRecordField(key, Object.keys(obj)))
+          },
+          set() {
+            throw new TypeError(errAttemptedFieldMutation)
+          }
+        })
+      }
 
-    // If our environment supports proxies, use them to prevent accessing
-    // nonexistent fields
-    if (typeof Proxy === 'function') {
-      return new Proxy(Object.freeze(obj), {
-        get(_, key) {
-          if (key in obj)
-            return obj[key]
-          else
-            throw new TypeError(errGetNonexistentRecordField(key, Object.keys(obj)))
-        },
-        set() {
-          throw new TypeError(errAttemptedFieldMutation)
-        }
-      })
-    }
-
-    // Second best option: freeze it
-    if (typeof Object.freeze === 'function') {
-      return Object.freeze(obj)
+      // Second best option: freeze it
+      if (typeof Object.freeze === 'function')
+        return Object.freeze(obj)
     }
 
     return obj
