65cec4a396a30e49a542f79bbb61b14f1ab88ad9
Tom Crockett <pelotom@gmail.com>
2016-05-27 09:13:04 -0700

    Document testSetup.js

--- testSetup.js
+++ testSetup.js
@@ -7,10 +7,13 @@ const { CLIEngine } = require('eslint')
 const cli = new CLIEngine()
 const { results } = cli.executeOnFiles(['src'])
 const formatter = cli.getFormatter('stylish')
-console.log(formatter(results).replace(/\n/g, '\n  '))
+// Indent results to align with mocha output
+const summary = formatter(results).replace(/\n/g, '\n  ')
+console.log(summary)
 if (CLIEngine.getErrorResults(results).length) {
-  console.error('  Skipping tests due to lint errors')
+  // Throw to short-circuit the test suite if we have linting errors
   const err = new Error()
+  // Don't allow Mocha to print a useless stack trace
   err.stack = ''
   throw err
 }
