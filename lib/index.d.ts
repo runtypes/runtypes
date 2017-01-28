4cd0891d70686df921418dc7777999e97b035a09
Tom Crockett <pelotom@gmail.com>
2017-01-27 16:53:55 -0800

    New build

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -29,25 +29,25 @@ export declare type Failure = {
  */
 export declare type Result<A> = Success<A> | Failure;
 /**
- * A validator determines whether a value conforms to a type specification.
+ * A runtype determines at runtime whether a value conforms to a type specification.
  */
-export declare type Validator<A> = {
+export declare type Runtype<A> = {
     /**
-     * Attempts to cast a value to the type for this validator and return it.
+     * Attempts to cast a value to the type for this runtype and return it.
      * Throws an exception if validation fails.
      */
     coerce(x: {}): A;
     /**
-     * Validates that a value conforms to the type of this validator, and
-     * returns a result indicating success or failure (does not throw).
+     * Validates that a value conforms to this type, and returns a result indicating
+     * success or failure (does not throw).
      */
     validate(x: {}): Result<A>;
     /**
-     * A type guard for the type that this validator validates.
+     * A type guard for this runtype.
      */
     guard(x: {}): x is A;
     /**
-     * Provides a way to reference the constructed type that this validator
+     * Provides a way to reference the constructed type that this runtype
      * validates.
      */
     falseWitness: A;
@@ -55,55 +55,55 @@ export declare type Validator<A> = {
 /**
  * Validates anything, but provides no new type information about it.
  */
-export declare const anything: Validator<{}>;
+export declare const anything: Runtype<{}>;
 /**
  * Validates nothing (always fails).
  */
-export declare const nothing: Validator<never>;
+export declare const nothing: Runtype<never>;
 /**
  * Validates that a value is a boolean.
  */
-export declare const boolean: Validator<boolean>;
+export declare const boolean: Runtype<boolean>;
 /**
  * Validates that a value is a number.
  */
-export declare const number: Validator<number>;
+export declare const number: Runtype<number>;
 /**
  * Validates that a value is a string.
  */
-export declare const string: Validator<string>;
+export declare const string: Runtype<string>;
 /**
- * Construct a validator of literals.
+ * Construct a literal runtype.
  */
-export declare function literal<K extends string | number | boolean>(l: K): Validator<K>;
+export declare function literal<K extends string | number | boolean>(l: K): Runtype<K>;
 /**
- * Construct a validator of arrays from a validator for its elements.
+ * Construct an array runtype from a runtype for its elements.
  */
-export declare function array<A>(v: Validator<A>): Validator<A[]>;
+export declare function array<A>(v: Runtype<A>): Runtype<A[]>;
 /**
- * Construct a validator of tuples from validators for its elements.
+ * Construct a tuple runtype from runtypes for each of its elements.
  */
-export declare function tuple<A>(a: Validator<A>, strict?: boolean): Validator<[A]>;
-export declare function tuple<A, B>(a: Validator<A>, b: Validator<B>, strict?: boolean): Validator<[A, B]>;
-export declare function tuple<A, B, C>(a: Validator<A>, b: Validator<B>, c: Validator<C>, strict?: boolean): Validator<[A, B, C]>;
-export declare function tuple<A, B, C, D>(a: Validator<A>, b: Validator<B>, c: Validator<C>, d: Validator<D>, strict?: boolean): Validator<[A, B, C, D]>;
-export declare function tuple<A, B, C, D, E>(a: Validator<A>, b: Validator<B>, c: Validator<C>, d: Validator<D>, e: Validator<E>, strict?: boolean): Validator<[A, B, C, D, E]>;
-export declare function tuple<A, B, C, D, E, F>(a: Validator<A>, b: Validator<B>, c: Validator<C>, d: Validator<D>, e: Validator<E>, f: Validator<F>, strict?: boolean): Validator<[A, B, C, D, E, F]>;
-export declare function tuple<A, B, C, D, E, F, G>(a: Validator<A>, b: Validator<B>, c: Validator<C>, d: Validator<D>, e: Validator<E>, f: Validator<F>, g: Validator<G>, strict?: boolean): Validator<[A, B, C, D, E, F, G]>;
+export declare function tuple<A>(a: Runtype<A>, strict?: boolean): Runtype<[A]>;
+export declare function tuple<A, B>(a: Runtype<A>, b: Runtype<B>, strict?: boolean): Runtype<[A, B]>;
+export declare function tuple<A, B, C>(a: Runtype<A>, b: Runtype<B>, c: Runtype<C>, strict?: boolean): Runtype<[A, B, C]>;
+export declare function tuple<A, B, C, D>(a: Runtype<A>, b: Runtype<B>, c: Runtype<C>, d: Runtype<D>, strict?: boolean): Runtype<[A, B, C, D]>;
+export declare function tuple<A, B, C, D, E>(a: Runtype<A>, b: Runtype<B>, c: Runtype<C>, d: Runtype<D>, e: Runtype<E>, strict?: boolean): Runtype<[A, B, C, D, E]>;
+export declare function tuple<A, B, C, D, E, F>(a: Runtype<A>, b: Runtype<B>, c: Runtype<C>, d: Runtype<D>, e: Runtype<E>, f: Runtype<F>, strict?: boolean): Runtype<[A, B, C, D, E, F]>;
+export declare function tuple<A, B, C, D, E, F, G>(a: Runtype<A>, b: Runtype<B>, c: Runtype<C>, d: Runtype<D>, e: Runtype<E>, f: Runtype<F>, g: Runtype<G>, strict?: boolean): Runtype<[A, B, C, D, E, F, G]>;
 /**
- * Construct a validator of records from validators for its values.
+ * Construct a record runtype from runtypes for its values.
  */
-export declare function record<O>(validators: {
-    [K in keyof O]: Validator<O[K]>;
-}): Validator<O>;
+export declare function record<O>(runtypes: {
+    [K in keyof O]: Runtype<O[K]>;
+}): Runtype<O>;
 /**
- * Construct a validator of unions from validators for its alternatives.
+ * Construct a union runtype from runtypes for its alternatives.
  */
-export declare function union(): Validator<never>;
-export declare function union<A>(a: Validator<A>): Validator<A>;
-export declare function union<A, B>(a: Validator<A>, b: Validator<B>): Validator<A | B>;
-export declare function union<A, B, C>(a: Validator<A>, b: Validator<B>, c: Validator<C>): Validator<A | B | C>;
-export declare function union<A, B, C, D>(a: Validator<A>, b: Validator<B>, c: Validator<C>, d: Validator<D>): Validator<A | B | C | D>;
-export declare function union<A, B, C, D, E>(a: Validator<A>, b: Validator<B>, c: Validator<C>, d: Validator<D>, e: Validator<E>): Validator<A | B | C | D | E>;
-export declare function union<A, B, C, D, E, F>(a: Validator<A>, b: Validator<B>, c: Validator<C>, d: Validator<D>, e: Validator<E>, f: Validator<F>): Validator<A | B | C | D | E | F>;
-export declare function union<A, B, C, D, E, F, G>(a: Validator<A>, b: Validator<B>, c: Validator<C>, d: Validator<D>, e: Validator<E>, f: Validator<F>, g: Validator<G>): Validator<A | B | C | D | E | F | G>;
+export declare function union(): Runtype<never>;
+export declare function union<A>(a: Runtype<A>): Runtype<A>;
+export declare function union<A, B>(a: Runtype<A>, b: Runtype<B>): Runtype<A | B>;
+export declare function union<A, B, C>(a: Runtype<A>, b: Runtype<B>, c: Runtype<C>): Runtype<A | B | C>;
+export declare function union<A, B, C, D>(a: Runtype<A>, b: Runtype<B>, c: Runtype<C>, d: Runtype<D>): Runtype<A | B | C | D>;
+export declare function union<A, B, C, D, E>(a: Runtype<A>, b: Runtype<B>, c: Runtype<C>, d: Runtype<D>, e: Runtype<E>): Runtype<A | B | C | D | E>;
+export declare function union<A, B, C, D, E, F>(a: Runtype<A>, b: Runtype<B>, c: Runtype<C>, d: Runtype<D>, e: Runtype<E>, f: Runtype<F>): Runtype<A | B | C | D | E | F>;
+export declare function union<A, B, C, D, E, F, G>(a: Runtype<A>, b: Runtype<B>, c: Runtype<C>, d: Runtype<D>, e: Runtype<E>, f: Runtype<F>, g: Runtype<G>): Runtype<A | B | C | D | E | F | G>;
