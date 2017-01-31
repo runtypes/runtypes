24f31fc73a167d329a23f3a3c38d30e7ba7ea397
Tom Crockett <pelotom@gmail.com>
2017-01-31 15:00:58 -0800

    The validator really needs to take any (not {}) to account for null and undefined

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+
 /**
  * A successful validation result.
  */
@@ -41,18 +42,18 @@ export type Runtype<A> = {
    * Attempts to cast a value to the type for this runtype and return it.
    * Throws an exception if validation fails.
    */
-  coerce(x: {}): A
+  coerce(x: any): A
 
   /**
    * Validates that a value conforms to this type, and returns a result indicating
    * success or failure (does not throw).
    */
-  validate(x: {}): Result<A>
+  validate(x: any): Result<A>
 
   /**
    * A type guard for this runtype.
    */
-  guard(x: {}): x is A
+  guard(x: any): x is A
 
   /**
    * Provides a way to reference the constructed type that this runtype
