a4a5b7a860e538eb3b78047539c8295b7d8a1b92
Tom Crockett <pelotom@gmail.com>
2017-02-18 01:01:36 -0800

    Don't export hasKey

--- src/index.ts
+++ src/index.ts
@@ -1229,6 +1229,6 @@ class ValidationError extends Error {
 
 // Type guard to determine if an object has a given key
 // If this feature gets implemented, we can use `in` instead: https://github.com/Microsoft/TypeScript/issues/10485
-export function hasKey<K extends string>(k: K, o: {}): o is { [_ in K]: {} } {
+function hasKey<K extends string>(k: K, o: {}): o is { [_ in K]: {} } {
   return typeof o === 'object' && k in o
 }
