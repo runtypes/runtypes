bb916e3c770ee967b3187f3a1863876777ee61f8
Tom Crockett <pelotom@gmail.com>
2017-02-24 10:26:48 -0800

    Add an example of iterating over the values of a union of literals

---
+++ src/examples/union-values.ts
@@ -0,0 +1,15 @@
+import { Union, Literal } from '../index'
+
+const Day = Union(
+  Literal('Sunday'),
+  Literal('Monday'),
+  Literal('Tuesday'),
+  Literal('Wednesday'),
+  Literal('Thursday'),
+  Literal('Friday'),
+  Literal('Saturday'),
+)
+
+for (const day of Day.Alternatives.map(lit => lit.value)) {
+  console.log(`Good morning, it's ${day}!`)
+}
