f4d39f67dbedd52bae85ebf17dedacca63dc1cc5
Tom Crockett <pelotom@gmail.com>
2017-02-24 11:23:13 -0800

    No need to use Func vs Function now

--- src/types/function.ts
+++ src/types/function.ts
@@ -1,14 +1,13 @@
 import { Runtype, create } from '../runtype'
 import { ValidationError } from '../validation-error'
 
-interface Func extends Runtype<(...args: any[]) => any> { tag: 'function' }
+export interface Function extends Runtype<(...args: any[]) => any> { tag: 'function' }
 
 /**
  * Construct a runtype for functions.
  */
-const Func = create<Func>(x => {
+export const Function = create<Function>(x => {
   if (typeof x !== 'function')
     throw new ValidationError(`Expected a function but was ${typeof x}`)
   return x
 }, { tag: 'function' })
-export { Func as Function }
