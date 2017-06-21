f59bbe492e4a364b880c9e1b7bf46e74ae8d9448
Andrey Melnikov <parabellum06-94@yandex.ru>
2017-06-21 22:38:49 +0300

    fix(lib): drop stale files

--- lib/types/custom.d.ts
+++
@@ -1,7 +0,0 @@
-import { Runtype, Rt, Static } from '../runtype';
-export interface Custom<A extends Rt, T extends string, K> extends Runtype<Static<A>> {
-    tag: T;
-    underlying: A;
-    args: K;
-}
-export declare function Custom<A extends Rt, T extends string, K>(underlying: A, constraint: (x: Static<A>, args: K) => boolean | string, tag: T, args: K): Custom<A, T, K>;
