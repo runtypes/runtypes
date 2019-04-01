50e352636dbcfd1bf0d6c16d2c5b5be5ca2f2763
Miika Hänninen <miika.hanninen@reaktor.com>
2019-04-01 11:19:43 +0300

    Cover two missing cases in the tests

--- src/match.spec.ts
+++ src/match.spec.ts
@@ -1,4 +1,4 @@
-import { Literal, String, Number, match, Unknown } from '.';
+import { Literal, String, Number, match } from '.';
 
 describe('match', () => {
   it('works', () => {
@@ -6,17 +6,11 @@ describe('match', () => {
       [Literal(42), fortyTwo => fortyTwo / 2],
       [Number, n => n + 9],
       [String, s => s.length * 2],
-      [
-        Unknown,
-        () => {
-          throw new Error('woops');
-        },
-      ],
     );
 
     expect(f(42)).toBe(21);
     expect(f(16)).toBe(25);
     expect(f('yooo')).toBe(8);
-    expect(() => f(true)).toThrow('woops');
+    expect(() => f(true as any)).toThrow('No alternatives were matched');
   });
 });
