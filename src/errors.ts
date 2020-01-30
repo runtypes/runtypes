1028eb1dcd0f9b9bded3ef8da71a2051ff1876cc
Eugeny Schibrikov <eugeny.schibrikov@paxport.net>
2020-01-30 16:46:31 +0500

    Added failed key to error message

--- src/errors.ts
+++ src/errors.ts
@@ -2,7 +2,7 @@ export class ValidationError extends Error {
   public name: string = 'ValidationError';
 
   constructor(public message: string, public key?: string) {
-    super(message);
+    super(key ? `${message} in ${key}` : message);
 
     Object.setPrototypeOf(this, ValidationError.prototype);
   }
