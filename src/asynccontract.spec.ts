179f5689a4e849f47ece065b2a60ee2146b3a458
Sam Schick <sam-schick@pluralsight.com>
2020-01-31 16:47:44 -0500

    Added tests for additional coverage

--- src/asynccontract.spec.ts
+++ src/asynccontract.spec.ts
@@ -1,16 +1,29 @@
 import { AsyncContract, Number } from '.';
+import { ValidationError } from './errors';
 
 describe('AsyncContract', () => {
   describe('when function does not return a promise', () => {
     it('throws a validation error', () => {
       const contractedFunction = AsyncContract(Number).enforce(() => 7 as any);
-      expect(contractedFunction).toThrow();
+      expect(contractedFunction).toThrow(ValidationError);
+    });
+  });
+  describe('when a function does return a promise, but for the wrong type', () => {
+    it('throws a validation error asynchronously', async () => {
+      const contractedFunction = AsyncContract(Number).enforce(() => Promise.resolve('hi' as any));
+      await expect(contractedFunction()).rejects;
     });
   });
   describe('when a function does return a promise', () => {
-    it('should validate successfully', () => {
+    it('should validate successfully', async () => {
       const contractedFunction = AsyncContract(Number).enforce(() => Promise.resolve(7));
-      return contractedFunction();
+      await expect(contractedFunction()).resolves.toBe(7);
+    });
+  });
+  describe('when not enough arguments are provided', () => {
+    it('throws a validation error', () => {
+      const contractedFunction = AsyncContract(Number, Number).enforce(n => Promise.resolve(n + 1));
+      expect(contractedFunction).toThrow(ValidationError);
     });
   });
 });
