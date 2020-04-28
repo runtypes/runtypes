d57c797735bf8a9dbcd9d197d75d1b9042bd1b6d
Marius Danila <mdanila@netflix.com>
2020-04-28 15:07:23 -0700

    Add support for generating a browser-friendly single-file distribution

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /.*\.spec\..*/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'runtypes.js',
+  },
+  devtool: 'source-map',
+};
