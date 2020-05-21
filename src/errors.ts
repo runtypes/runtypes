c9d132107dcbec8f001c0b2279fa777b4099ed38
Richard Gieg <richgieg@users.noreply.github.com>
2020-05-21 16:40:44 -0700

    Fix ValidationError message bug (#134)
    * Fix ValidationError message bug
    
    * Include failed key in expected error messages in tests
    
    * Fix formatting in index.spec.ts

--- src/errors.ts
+++ src/errors.ts
@@ -1,7 +1,7 @@
 export class ValidationError extends Error {
   public name: string = 'ValidationError';
 
-  constructor(public message: string, public key?: string) {
+  constructor(message: string, public key?: string) {
     super(key ? `${message} in ${key}` : message);
 
     Object.setPrototypeOf(this, ValidationError.prototype);
