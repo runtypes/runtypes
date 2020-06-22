c90580603e0217f689f11e32db1b2e083fe0748a
Ivan Kerin <ikerin@gmail.com>
2020-06-22 20:32:39 +0300

    Implement Discriminated Unions (#149)
    * Implement Discriminated Unions
    
    If:
    - all the alternatives of a union are records
    - they have at least one field in common, that is a literal in each
    record with unique values
    
    Then when checking the alternatives we can pick the correct one first,
    and then perform the validation, thus producing more specific error
    messages.
    
    This is something already present in TypeScript itself https://www.typescriptlang.org/docs/handbook/advanced-types.html#discriminated-unions
    Though this implementation would not work in all the cases covered by
    TS, but its a start.
    
    * Remove perf optimisation

--- src/types/union.spec.ts
+++ src/types/union.spec.ts
@@ -1,4 +1,4 @@
-import { Union, String, Literal } from '..';
+import { Union, String, Literal, Record, Number, InstanceOf } from '..';
 
 const ThreeOrString = Union(Literal(3), String);
 
@@ -11,6 +11,52 @@ describe('union', () => {
       );
       expect(match(3)).toBe(8);
       expect(match('hello')).toBe(20);
+    });
+  });
+
+  describe('discriminated union', () => {
+    it('should pick correct alternative with typescript docs example', () => {
+      const Square = Record({ kind: Literal('square'), size: Number });
+      const Rectangle = Record({ kind: Literal('rectangle'), width: Number, height: Number });
+      const Circle = Record({ kind: Literal('circle'), radius: Number });
+
+      const Shape = Union(Square, Rectangle, Circle);
+
+      expect(Shape.validate({ kind: 'square', size: new Date() })).toMatchObject({
+        success: false,
+        key: 'size',
+      });
+
+      expect(Shape.validate({ kind: 'rectangle', size: new Date() })).toMatchObject({
+        success: false,
+        key: 'width',
+      });
+
+      expect(Shape.validate({ kind: 'circle', size: new Date() })).toMatchObject({
+        success: false,
+        key: 'radius',
+      });
+
+      expect(Shape.validate({ kind: 'other', size: new Date() })).not.toHaveProperty('key');
+    });
+
+    it('hould not pick alternative if the discriminant is not unique', () => {
+      const Square = Record({ kind: Literal('square'), size: Number });
+      const Rectangle = Record({ kind: Literal('rectangle'), width: Number, height: Number });
+      const CircularSquare = Record({ kind: Literal('square'), radius: Number });
+
+      const Shape = Union(Square, Rectangle, CircularSquare);
+
+      expect(Shape.validate({ kind: 'square', size: new Date() })).not.toHaveProperty('key');
+    });
+
+    it('should not pick alternative if not all types are records', () => {
+      const Square = Record({ kind: Literal('square'), size: Number });
+      const Rectangle = Record({ kind: Literal('rectangle'), width: Number, height: Number });
+
+      const Shape = Union(Square, Rectangle, InstanceOf(Date));
+
+      expect(Shape.validate({ kind: 'square', size: new Date() })).not.toHaveProperty('key');
     });
   });
 });
