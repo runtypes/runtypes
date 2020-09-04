466f3dfe64b08aa85165bf91ab4541e3125abc29
Forbes Lindesay <forbes@lindesay.co.uk>
2020-09-04 20:52:39 +0100

    feat: arbitrary length tuples (#5)
    BREAKING CHANGE: the various `Tuple2`, `Tuple3`, ... types have all been replaced with a single `Tuple` type that takes a tuple of Runtype validators.

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -16,7 +16,6 @@ import {
   Record,
   Partial as RTPartial,
   Tuple,
-  Tuple2,
   Union,
   Intersect,
   Function,
@@ -754,7 +753,7 @@ describe('change static type with Constraint', () => {
     | Record<{ [_ in string]: Reflect }, true>
     | RTPartial<{ [_ in string]: Reflect }, false>
     | RTPartial<{ [_ in string]: Reflect }, true>
-    | Tuple2<Reflect, Reflect>
+    | Tuple<[Reflect, Reflect]>
     | Union<[Reflect, Reflect]>
     | Intersect<[Reflect, Reflect]>
     | Function
