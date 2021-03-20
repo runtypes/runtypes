ab8c1163424e39124c3d07a261378d7de04ec49a
Yu Shimura <mail@yuhr.org>
2021-03-21 03:24:57 +0900

    refactor: use `bigint` type instead of `BigInt` interface

--- src/types/bigint.ts
+++ src/types/bigint.ts
@@ -1,21 +1,19 @@
 import { Runtype, create } from '../runtype';
 
-export interface _BigInt extends Runtype<BigInt> {
+export interface BigInt extends Runtype<bigint> {
   tag: 'bigint';
-};
+}
 
 /**
  * Validates that a value is a bigint.
  */
-const _BigInt = create<_BigInt>(
+export const BigInt = create<BigInt>(
   value =>
     typeof value === 'bigint'
       ? { success: true, value }
       : {
-        success: false,
-        message: `Expected bigint, but was ${value === null ? value : typeof value}`,
-      },
+          success: false,
+          message: `Expected bigint, but was ${value === null ? value : typeof value}`,
+        },
   { tag: 'bigint' },
 );
-
-export { _BigInt as BigInt };
