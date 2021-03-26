138447b22d2a72c7158feda48e0538d08c8d0139
Yu Shimura <mail@yuhr.org>
2021-03-26 23:18:46 +0900

    feat: support validation against non-keyed symbols

--- src/types/symbol.ts
+++ src/types/symbol.ts
@@ -6,35 +6,30 @@ export interface Symbol extends Runtype<symbol> {
     Validates that a value is a symbol.
     @param {string} key - Specify what key the symbol is for.
    */
-  <K extends string>(key: K): SymbolFor<K>;
+  <K extends string | undefined>(key: K): SymbolFor<K>;
 }
 
-export interface SymbolFor<K extends string> extends Runtype<symbol> {
+export interface SymbolFor<K extends string | undefined> extends Runtype<symbol> {
   tag: 'symbol';
   key: K;
 }
 
-const f = (key: string) =>
+const f = (key: string | undefined) =>
   create<Symbol>(
     value => {
       if (typeof value !== 'symbol') {
         return {
           success: false,
-          message: `Expected symbol, but was ${
-            value === 'null' ? value : Array.isArray(value) ? 'array' : typeof value
-          }`,
+          message: `Expected symbol, but was ${typeStringOf(value)}`,
         };
       } else {
-        const keyOfValue = global.Symbol.keyFor(value);
-        if (keyOfValue === undefined) {
+        const keyForValue = global.Symbol.keyFor(value);
+        if (keyForValue !== key) {
           return {
             success: false,
-            message: `Expected symbol key to be "${key}", but was undefined`,
-          };
-        } else if (keyOfValue !== key) {
-          return {
-            success: false,
-            message: `Expected symbol key to be "${key}", but was "${keyOfValue}"`,
+            message: `Expected symbol key to be ${quoteIfPresent(key)}, but was ${quoteIfPresent(
+              keyForValue,
+            )}`,
           };
         } else {
           return { success: true, value };
@@ -47,15 +42,18 @@ const f = (key: string) =>
 /**
  * Validates that a value is a symbol.
  */
-export const Symbol = create<Symbol>(
-  value =>
-    typeof value === 'symbol'
-      ? { success: true, value }
-      : {
-          success: false,
-          message: `Expected symbol, but was ${
-            value === 'null' ? value : Array.isArray(value) ? 'array' : typeof value
-          }`,
-        },
-  Object.assign(f, { tag: 'symbol' }),
-);
+export const Symbol = create<Symbol>(value => {
+  if (typeof value !== 'symbol') {
+    return {
+      success: false,
+      message: `Expected symbol, but was ${typeStringOf(value)}`,
+    };
+  } else {
+    return { success: true, value };
+  }
+}, Object.assign(f, { tag: 'symbol' }));
+
+const quoteIfPresent = (key: string | undefined) => (key === undefined ? 'undefined' : `"${key}"`);
+
+const typeStringOf = (value: unknown) =>
+  value === null ? 'null' : Array.isArray(value) ? 'array' : typeof value;
