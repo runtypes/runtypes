0a85158ba95bad1e46e41546e58ae173fae27e8d
Yu Shimura <mail@yuhr.org>
2021-03-26 18:25:56 +0900

    feat: add symbol key validation

--- src/types/symbol.ts
+++ src/types/symbol.ts
@@ -1,21 +1,61 @@
 import { Runtype, create } from '../runtype';
 
-interface Sym extends Runtype<symbol> {
+export interface Symbol extends Runtype<symbol> {
   tag: 'symbol';
+  /**
+    Validates that a value is a symbol.
+    @param {string} key - Specify what key the symbol is for.
+   */
+  <K extends string>(key: K): SymbolFor<K>;
 }
+
+export interface SymbolFor<K extends string> extends Runtype<symbol> {
+  tag: 'symbol';
+  key: K;
+}
+
+const f = (key: string) =>
+  create<Symbol>(
+    value => {
+      if (typeof value !== 'symbol') {
+        return {
+          success: false,
+          message: `Expected symbol, but was ${
+            value === 'null' ? value : Array.isArray(value) ? 'array' : typeof value
+          }`,
+        };
+      } else {
+        const keyOfValue = global.Symbol.keyFor(value);
+        if (keyOfValue === undefined) {
+          return {
+            success: false,
+            message: `Expected symbol key to be "${key}", but was undefined`,
+          };
+        } else if (keyOfValue !== key) {
+          return {
+            success: false,
+            message: `Expected symbol key to be "${key}", but was "${keyOfValue}"`,
+          };
+        } else {
+          return { success: true, value };
+        }
+      }
+    },
+    { tag: 'symbol', key },
+  );
 
 /**
  * Validates that a value is a symbol.
  */
-const Sym = create<Sym>(
+export const Symbol = create<Symbol>(
   value =>
     typeof value === 'symbol'
       ? { success: true, value }
       : {
           success: false,
-          message: `Expected symbol, but was ${value === null ? value : typeof value}`,
+          message: `Expected symbol, but was ${
+            value === 'null' ? value : Array.isArray(value) ? 'array' : typeof value
+          }`,
         },
-  { tag: 'symbol' },
+  Object.assign(f, { tag: 'symbol' }),
 );
-
-export { Sym as Symbol };
