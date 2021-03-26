142573062a67b2fa748f31c74cc3c0dd35b47146
Yu Shimura <mail@yuhr.org>
2021-03-26 18:33:53 +0900

    fix: update reflect variants for `Symbol`

--- src/reflect.ts
+++ src/reflect.ts
@@ -11,7 +11,8 @@ export type Reflect =
   | ({ tag: 'number' } & Runtype<number>)
   | ({ tag: 'bigint' } & Runtype<bigint>)
   | ({ tag: 'string' } & Runtype<string>)
-  | ({ tag: 'symbol' } & Runtype<symbol>)
+  | ({ tag: 'symbol'; key: string } & Runtype<symbol>)
+  | ({ tag: 'symbol'; (key: string): Runtype<symbol> } & Runtype<symbol>)
   | ({ tag: 'literal'; value: LiteralBase } & Runtype<LiteralBase>)
   | ({ tag: 'array'; element: Reflect; isReadonly: boolean } & Runtype<ReadonlyArray<unknown>>)
   | ({
