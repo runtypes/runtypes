846d0182b86bccfb0d578a42dc4318f9cf6c7eab
Yu Shimura <mail@yuhr.org>
2021-03-29 04:01:32 +0900

    Remove unnecessary code from `Brand`

--- src/types/brand.ts
+++ src/types/brand.ts
@@ -18,17 +18,9 @@ export interface Brand<B extends string, A extends Runtype>
 }
 
 export function Brand<B extends string, A extends Runtype>(brand: B, entity: A) {
-  return create<Brand<B, A>>(
-    value => {
-      const validated = entity.validate(value);
-      return validated.success
-        ? { success: true, value: validated.value as Static<Brand<B, A>> }
-        : validated;
-    },
-    {
-      tag: 'brand',
-      brand,
-      entity,
-    },
-  );
+  return create<any>(value => entity.validate(value), {
+    tag: 'brand',
+    brand,
+    entity,
+  });
 }
