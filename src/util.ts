789538e2a91b0425a3cf38e9086557d5b22b1e82
Yu Shimura <mail@yuhr.org>
2021-03-29 07:38:04 +0900

    refactor: use descriptive variable names

--- src/util.ts
+++ src/util.ts
@@ -1,10 +1,10 @@
 // Type guard to determine if an object has a given key
 // If this feature gets implemented, we can use `in` instead: https://github.com/Microsoft/TypeScript/issues/10485
 export function hasKey<K extends string | number | symbol>(
-  k: K,
-  o: unknown,
-): o is { [_ in K]: unknown } {
-  return typeof o === 'object' && o !== null && k in o;
+  key: K,
+  object: unknown,
+): object is { [_ in K]: unknown } {
+  return typeof object === 'object' && object !== null && key in object;
 }
 
 export const typeOf = (value: unknown) =>
