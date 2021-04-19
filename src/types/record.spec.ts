0b8b0e6576b9a34c27bc886ab0d5d2cf11e41439
Yu Shimura <mail@yuhr.org>
2021-04-20 00:34:15 +0900

    test: enhance tests

--- src/types/record.spec.ts
+++ src/types/record.spec.ts
@@ -1,4 +1,4 @@
-import { Record, String } from '..';
+import { Record, String, Static } from '..';
 
 describe('record', () => {
   const CrewMember = Record({
@@ -9,17 +9,23 @@ describe('record', () => {
 
   describe('pick', () => {
     const PetMember = CrewMember.pick('name', 'home');
+    type PetMember = Static<typeof PetMember>;
+    const petMember: PetMember = { name: '', home: '' };
 
     it('keeps only selected fields', () => {
       expect(Object.keys(PetMember.fields)).toEqual(['name', 'home']);
+      expect(PetMember.guard(petMember)).toBe(true);
     });
   });
 
   describe('omit', () => {
     const PetMember = CrewMember.omit('name', 'home');
+    type PetMember = Static<typeof PetMember>;
+    const petMember: PetMember = { rank: '' };
 
     it('drop selected fields', () => {
       expect(Object.keys(PetMember.fields)).toEqual(['rank']);
+      expect(PetMember.guard(petMember)).toBe(true);
     });
   });
 });
