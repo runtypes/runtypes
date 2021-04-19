0d97a97a5b3093a16a5fd08643f6ce87ce93a973
Yu Shimura <mail@yuhr.org>
2021-04-20 02:08:41 +0900

    test: add cases for zero arguments

--- src/types/record.spec.ts
+++ src/types/record.spec.ts
@@ -8,23 +8,35 @@ describe('record', () => {
   });
 
   describe('pick', () => {
-    const PetMember = CrewMember.pick('name', 'home');
-    type PetMember = Static<typeof PetMember>;
-    const petMember: PetMember = { name: '', home: '' };
-
     it('keeps only selected fields', () => {
+      const PetMember = CrewMember.pick('name', 'home');
+      type PetMember = Static<typeof PetMember>;
+      const petMember: PetMember = { name: '', home: '' };
       expect(Object.keys(PetMember.fields)).toEqual(['name', 'home']);
+      expect(PetMember.guard(petMember)).toBe(true);
+    });
+    it('works with empty arguments', () => {
+      const PetMember = CrewMember.pick();
+      type PetMember = Static<typeof PetMember>;
+      const petMember: PetMember = {};
+      expect(Object.keys(PetMember.fields)).toEqual([]);
       expect(PetMember.guard(petMember)).toBe(true);
     });
   });
 
   describe('omit', () => {
-    const PetMember = CrewMember.omit('name', 'home');
-    type PetMember = Static<typeof PetMember>;
-    const petMember: PetMember = { rank: '' };
-
     it('drop selected fields', () => {
+      const PetMember = CrewMember.omit('name', 'home');
+      type PetMember = Static<typeof PetMember>;
+      const petMember: PetMember = { rank: '' };
       expect(Object.keys(PetMember.fields)).toEqual(['rank']);
+      expect(PetMember.guard(petMember)).toBe(true);
+    });
+    it('works with empty arguments', () => {
+      const PetMember = CrewMember.omit();
+      type PetMember = Static<typeof PetMember>;
+      const petMember: PetMember = { name: '', home: '', rank: '' };
+      expect(Object.keys(PetMember.fields)).toEqual(['name', 'rank', 'home']);
       expect(PetMember.guard(petMember)).toBe(true);
     });
   });
