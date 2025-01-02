0317c921775c91a1ba08d544d67b78ccdb6a315e
Sʜɪᴍᴜʀᴀ Yū <mail@yuhr.org>
2025-01-03 04:15:09 +0900

    perf: reduce cost of `hasKey`

--- src/utils-internal/hasKey.ts
+++ src/utils-internal/hasKey.ts
@@ -1,9 +1,7 @@
-import isObject from "./isObject.ts"
-
 const hasKey = <K extends string | number | symbol>(
 	key: K,
-	object: unknown,
+	object: object,
 ): object is { [_ in K]: unknown } =>
-	isObject(object) && globalThis.Object.prototype.propertyIsEnumerable.call(object, key)
+	globalThis.Object.prototype.propertyIsEnumerable.call(object, key)
 
 export default hasKey
