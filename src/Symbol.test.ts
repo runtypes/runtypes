ad56044cd8452f70add4242b6a308626c68e148f
Sʜɪᴍᴜʀᴀ Yū <mail@yuhr.org>
2025-01-06 04:54:40 +0900

    refactor!: remove `SymbolFor`

--- src/Symbol.test.ts
+++ src/Symbol.test.ts
@@ -1,13 +1,34 @@
 import Symbol from "./Symbol.ts"
-import { assert } from "@std/assert"
+import Failcode from "./result/Failcode.ts"
+import { assert, assertEquals, assertObjectMatch } from "@std/assert"
 
 Deno.test("Symbol", async t => {
 	await t.step("without key", async t => {
-		const s = globalThis.Symbol()
-		assert(Symbol.guard(s))
+		assert(Symbol.guard(globalThis.Symbol()))
+		assert(Symbol.guard(globalThis.Symbol.for("key")))
 	})
 	await t.step("with key", async t => {
-		const s = globalThis.Symbol.for("key")
-		assert(Symbol("key").guard(s))
+		assert(Symbol("key").guard(globalThis.Symbol.for("key")))
+		assert(!Symbol("key").guard(globalThis.Symbol()))
+	})
+
+	await t.step("symbol", async t => {
+		assertObjectMatch(Symbol, { tag: "symbol" })
+		assertObjectMatch(Symbol("runtypes"), { tag: "symbol", key: "runtypes" })
+		assertEquals(Symbol("runtypes?").validate(globalThis.Symbol.for("runtypes!")), {
+			success: false,
+			code: Failcode.VALUE_INCORRECT,
+			message: 'Expected symbol for "runtypes?", but was for "runtypes!"',
+		})
+		assertEquals(Symbol(undefined).validate(globalThis.Symbol.for("undefined")), {
+			success: false,
+			code: Failcode.VALUE_INCORRECT,
+			message: 'Expected unique symbol, but was for "undefined"',
+		})
+		assertEquals(Symbol("undefined").validate(globalThis.Symbol()), {
+			success: false,
+			code: Failcode.VALUE_INCORRECT,
+			message: 'Expected symbol for "undefined", but was unique',
+		})
 	})
 })
--- src/Symbol.ts
+++ src/Symbol.ts
@@ -2,32 +2,34 @@ import Runtype from "./Runtype.ts"
 import FAILURE from "./utils-internal/FAILURE.ts"
 import SUCCESS from "./utils-internal/SUCCESS.ts"
 
-interface Symbol extends Runtype.Common<symbol> {
+interface Symbol<K extends string | undefined = undefined> extends Runtype.Common<symbol> {
 	tag: "symbol"
+	key: K
+
 	/**
 	Validates that a value is a symbol with a specific key or without any key.
 	@param {string | undefined} key - Specify what key the symbol is for. If you want to ensure the validated symbol is *not* keyed, pass `undefined`.
 	*/
-	<K extends string | undefined>(key: K): SymbolFor<K>
-}
-
-interface SymbolFor<K extends string | undefined> extends Runtype.Common<symbol> {
-	tag: "symbol"
-	key: K
+	<K extends string | undefined>(key: K): Symbol<K>
 }
 
 const SymbolFor = <K extends string | undefined>(key: K) =>
-	Runtype.create<SymbolFor<K>>(
+	Runtype.create<Symbol<K>>(
 		(value, innerValidate, self) => {
 			if (typeof value !== "symbol") return FAILURE.TYPE_INCORRECT(self, value)
 			else {
 				const keyForValue = globalThis.Symbol.keyFor(value)
-				if (keyForValue !== key)
-					return FAILURE.VALUE_INCORRECT(
-						"symbol key",
-						quoteIfPresent(key),
-						quoteIfPresent(keyForValue),
-					)
+				console.log(keyForValue, self.key)
+				if (keyForValue !== self.key)
+					if (self.key === undefined) {
+						return FAILURE.VALUE_INCORRECT("unique", "symbol", `for ${quoteIfPresent(keyForValue)}`)
+					} else {
+						return FAILURE.VALUE_INCORRECT(
+							"symbol for",
+							quoteIfPresent(self.key),
+							keyForValue === undefined ? "unique" : `for ${quoteIfPresent(keyForValue)}`,
+						)
+					}
 				else return SUCCESS(value)
 			}
 		},
@@ -40,7 +42,7 @@ const SymbolFor = <K extends string | undefined>(key: K) =>
 const Symbol = Runtype.create<Symbol>(
 	(value, innerValidate, self) =>
 		typeof value === "symbol" ? SUCCESS(value) : FAILURE.TYPE_INCORRECT(self, value),
-	globalThis.Object.assign(SymbolFor, { tag: "symbol" as const }),
+	globalThis.Object.assign(SymbolFor, { tag: "symbol" as const, key: undefined }),
 )
 
 const quoteIfPresent = (key: string | undefined) => (key === undefined ? "undefined" : `"${key}"`)
