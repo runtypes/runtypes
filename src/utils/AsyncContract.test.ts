5c7628bd7c76e3ec0c5c1a37e4bd71a707ca538c
Sʜɪᴍᴜʀᴀ Yū <mail@yuhr.org>
2025-01-05 20:20:39 +0900

    feat!: rework contract APIs (#403)

--- src/utils/AsyncContract.test.ts
+++ src/utils/AsyncContract.test.ts
@@ -1,36 +1,77 @@
 import AsyncContract from "./AsyncContract.ts"
+import Array from "../Array.ts"
+import Literal from "../Literal.ts"
 import Number from "../Number.ts"
+import String from "../String.ts"
+import Tuple from "../Tuple.ts"
+import Unknown from "../Unknown.ts"
 import ValidationError from "../result/ValidationError.ts"
-import { assert, assertThrows } from "@std/assert"
+import { assertEquals, assertRejects } from "@std/assert"
 
 Deno.test("AsyncContract", async t => {
 	await t.step("when function does not return a promise", async t => {
 		await t.step("throws a validation error", async t => {
-			const contractedFunction = AsyncContract(Number).enforce(() => 7 as any)
-			assertThrows(contractedFunction, ValidationError)
+			const contractedFunction = AsyncContract({ receives: Tuple(), returns: Number }).enforce(
+				() =>
+					// @ts-expect-error: should fail
+					7,
+			)
+			assertRejects(contractedFunction, ValidationError)
 		})
 	})
-	await t.step("when a function does return a promise, but for the wrong type", async t => {
+	await t.step("when a function does return a promise, but of a wrong awaited type", async t => {
 		await t.step("throws a validation error asynchronously", async t => {
-			const contractedFunction = AsyncContract(Number).enforce(() => Promise.resolve("hi" as any))
-			try {
-				await contractedFunction()
-				assert(false)
-			} catch (e) {
-				assert(e instanceof ValidationError)
-			}
+			const contractedFunction = AsyncContract({ receives: Tuple(), returns: Number }).enforce(() =>
+				// @ts-expect-error: should fail
+				Promise.resolve("hi"),
+			)
+			assertRejects(contractedFunction, ValidationError)
 		})
 	})
 	await t.step("when a function does return a promise", async t => {
 		await t.step("should validate successfully", async () => {
-			const contractedFunction = AsyncContract(Number).enforce(() => Promise.resolve(7))
-			assert((await contractedFunction()) === 7)
+			const contractedFunction = AsyncContract({ receives: Tuple(), returns: Number }).enforce(() =>
+				Promise.resolve(7),
+			)
+			assertEquals(await contractedFunction(), 7)
 		})
 	})
 	await t.step("when not enough arguments are provided", async t => {
 		await t.step("throws a validation error", async t => {
-			const contractedFunction = AsyncContract(Number, Number).enforce(n => Promise.resolve(n + 1))
-			assertThrows(contractedFunction, ValidationError)
+			const contractedFunction = AsyncContract({
+				receives: Tuple(Number),
+				returns: Number,
+			}).enforce(n => Promise.resolve(n + 1))
+			// @ts-expect-error: should fail
+			assertRejects(contractedFunction, ValidationError)
 		})
+	})
+	await t.step("infers the parameter types", async t => {
+		const contractedFunction = AsyncContract({
+			receives: Tuple(Literal(0)),
+			returns: Unknown,
+		}).enforce((...args: readonly number[]) => Promise.resolve(7 as const))
+		assertEquals<7>(await contractedFunction(0), 7)
+		// @ts-expect-error: should fail
+		assertRejects(async () => await contractedFunction(1), ValidationError)
+	})
+	await t.step("infers the returned type", async t => {
+		const contractedFunction = AsyncContract({
+			receives: Array(Unknown),
+			returns: Unknown,
+		}).enforce(() => Promise.resolve(7 as const))
+		assertEquals<7>(await contractedFunction(), 7)
+	})
+	await t.step("should parse arguments and return values", async t => {
+		const ParseInt = String.withParser(parseInt)
+		const contractedFunction = AsyncContract({
+			receives: Array(ParseInt),
+			returns: Array(ParseInt),
+		}).enforce(async (...args) => args.map(globalThis.String))
+		assertEquals(await contractedFunction("42", "24"), [42, 24])
+	})
+	await t.step("skips checking when created without runtypes", async t => {
+		const contractedFunction = AsyncContract({}).enforce(async (a: 42) => a)
+		assertEquals<42>(await contractedFunction(42), 42)
 	})
 })
--- src/utils/Contract.ts
+++ src/utils/Contract.ts
@@ -1,58 +1,65 @@
 import type Runtype from "../Runtype.ts"
-import { type Static } from "../Runtype.ts"
-import ValidationError from "../result/ValidationError.ts"
-import FAILURE from "../utils-internal/FAILURE.ts"
+import { type Static, type Parsed } from "../Runtype.ts"
 
-type Contract<A extends readonly Runtype.Core[], R extends Runtype.Core> = {
-	enforce(
-		f: (
-			...args: {
-				[K in keyof A]: A[K] extends Runtype.Core ? Static<A[K]> : unknown
-			} & ArrayLike<unknown>
-		) => Static<R>,
-	): (
-		...args: {
-			[K in keyof A]: A[K] extends Runtype.Core ? Static<A[K]> : unknown
-		} & ArrayLike<unknown>
-	) => Static<R>
+type Options = {
+	receives?: Runtype.Core<readonly unknown[]> | undefined
+	returns?: Runtype.Core | undefined
 }
 
-const Contract: {
-	/**
-	 * Create a function contract.
-	 */
-	<P extends readonly Runtype.Core[], R extends Runtype.Core>(
-		...runtypes: [...P, R]
-	): Contract<P, R>
-} = <P extends readonly Runtype.Core[], R extends Runtype.Core>(
-	...runtypes: [...P, R]
-): Contract<P, R> => {
-	const lastIndex = runtypes.length - 1
-	const argRuntypes = runtypes.slice(0, lastIndex) as unknown as P
-	const returnRuntype = runtypes[lastIndex] as R
+type Function = (...args: readonly any[]) => any
+
+type EnforcedParametersStatic<
+	O extends Options,
+	F extends Function,
+> = O["receives"] extends Runtype.Core ? Static<O["receives"]> : Parameters<F>
+
+type EnforcedReturnTypeStatic<
+	O extends Options,
+	F extends Function,
+> = O["returns"] extends Runtype.Core ? Static<O["returns"]> : ReturnType<F>
+
+type EnforcedParametersParsed<
+	O extends Options,
+	F extends Function,
+> = O["receives"] extends Runtype.Core ? Parsed<O["receives"]> : Parameters<F>
+
+type EnforcedReturnTypeParsed<
+	O extends Options,
+	F extends Function,
+> = O["returns"] extends Runtype.Core ? Parsed<O["returns"]> & ReturnType<F> : ReturnType<F>
+
+type Contract<O extends Options> = O & {
+	enforce: <F extends (...args: EnforcedParametersParsed<O, F>) => EnforcedReturnTypeStatic<O, F>>(
+		f: F,
+	) => (...args: EnforcedParametersStatic<O, F>) => EnforcedReturnTypeParsed<O, F>
+}
+
+const parseReceived = <O extends Options, F extends Function>(
+	received: readonly unknown[],
+	receives: O["receives"],
+): EnforcedParametersParsed<O, F> => {
+	return (receives ? receives.parse(received) : received) as EnforcedParametersParsed<O, F>
+}
+
+const parseReturned = <O extends Options, F extends Function>(
+	returned: unknown,
+	returns: O["returns"],
+): EnforcedReturnTypeParsed<O, F> => {
+	return (returns ? returns.parse(returned) : returned) as EnforcedReturnTypeParsed<O, F>
+}
+
+/**
+ * Creates an  function contract.
+ */
+const Contract = <O extends Options>({ receives, returns }: O): Contract<O> => {
 	return {
+		receives,
+		returns,
 		enforce:
-			(
-				f: (
-					...args: {
-						[K in keyof P]: P[K] extends Runtype.Core ? Static<P[K]> : unknown
-					}
-				) => Static<R>,
-			) =>
-			(
-				...args: {
-					[K in keyof P]: P[K] extends Runtype.Core ? Static<P[K]> : unknown
-				}
-			): Static<R> => {
-				if (args.length < argRuntypes.length) {
-					const message = `Expected ${argRuntypes.length} arguments but only received ${args.length}`
-					const failure = FAILURE.ARGUMENT_INCORRECT(message)
-					throw new ValidationError(failure)
-				}
-				for (let i = 0; i < argRuntypes.length; i++) argRuntypes[i]!.check(args[i])
-				return returnRuntype.check(f(...args)) as Static<R>
-			},
-	}
+			f =>
+			(...args) =>
+				parseReturned(f(...(parseReceived(args, receives) as any)), returns),
+	} as Contract<O>
 }
 
 export default Contract
