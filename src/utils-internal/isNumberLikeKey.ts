9fb4b443d6e5479a09d56d273de3f4862e44eced
Sʜɪᴍᴜʀᴀ Yū <mail@yuhr.org>
2025-01-06 04:52:40 +0900

    fix: fix behavior of number-like keys in `Record` (#407)

--- src/utils-internal/isNumberLikeKey.ts
+++ src/utils-internal/isNumberLikeKey.ts
@@ -1,11 +1,9 @@
-const numberString = /^(?:-?\d+(?:\.\d+)?|NaN|-?Infinity)$/
-
 /**
  * Mimicking the behavior of type-level `` `${number}` ``.
  *
  * Note that `` `${NaN}` `` is not assignable to a variable of type `` `${number}` ``, but `` `${NaN as number}` `` is assignable, thus this function also accepts `"NaN"`. The same applies to `Infinity` and `-Infinity`.
  */
 const isNumberLikeKey = (key: string | symbol): key is `${number}` =>
-	typeof key === "string" && numberString.test(key)
+	typeof key === "string" && key === globalThis.Number(key).toString()
 
 export default isNumberLikeKey
