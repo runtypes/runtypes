7643b35cab39b0106ba08cd83b8345a87bc52e92
Sʜɪᴍᴜʀᴀ Yū <mail@yuhr.org>
2025-01-11 21:30:36 +0900

    fix: use brand string in error messages (#438)

--- src/utils-internal/FAILURE.ts
+++ src/utils-internal/FAILURE.ts
@@ -42,7 +42,7 @@ const FAILURE: {
 const toMessage = (failure: Failure): string => {
 	switch (failure.code) {
 		case Failcode.TYPE_INCORRECT:
-			return `Expected ${show(failure.expected)}, but was ${"details" in failure ? "incompatible" : typeOf(failure.received)}`
+			return `Expected ${show(failure.expected)}, but was ${"details" in failure || "inner" in failure ? "incompatible" : typeOf(failure.received)}`
 		case Failcode.VALUE_INCORRECT:
 			switch (failure.expected.tag) {
 				case "symbol": {
